0c60f90c1c605c947e6f89ab27144e7144da5416
Gentoli <gentoli@live.com>
2026-03-01 15:37:56 -0500

    revert storage

--- module-utilities.tf
+++ module-utilities.tf
@@ -31,3 +31,61 @@ module "utilities" {
   worker_drain_ignore_daemonsets = var.worker_drain_ignore_daemonsets
   worker_drain_timeout_seconds   = var.worker_drain_timeout_seconds
 }
+
+#fss
+module "storage" {
+  source = "./modules/storage"
+
+  # general oci parameters
+  tenancy_id          = local.tenancy_id
+  compartment_id      = local.compartment_id
+  availability_domain = var.availability_domains["fss"]
+  label_prefix        = var.label_prefix
+
+  # FSS network information
+  subnets      = var.subnets
+  assign_dns   = var.assign_dns
+  vcn_id       = local.vcn_id
+  nat_route_id = local.nat_route_id
+
+  fss_mount_path = var.fss_mount_path
+
+  # Export set configuration
+  max_fs_stat_bytes = var.max_fs_stat_bytes
+  max_fs_stat_files = var.max_fs_stat_files
+
+  providers = {
+    oci.home = oci.home
+  }
+
+  count = var.create_fss == true ? 1 : 0
+
+}
+
+#fss
+variable "create_fss" {
+  description = "Whether to enable provisioning for FSS"
+  default     = false
+  type        = bool
+}
+
+# fss mount path
+variable "fss_mount_path" {
+  description = "FSS mount path to be associated"
+  default     = "/oke_fss"
+  type        = string
+}
+
+# Controls the maximum tbytes, fbytes, and abytes, values reported by NFS FSSTAT calls through any associated mount targets.
+variable "max_fs_stat_bytes" {
+  description = "Maximum tbytes, fbytes, and abytes, values reported by NFS FSSTAT calls through any associated mount targets"
+  default     = 23843202333
+  type        = number
+}
+
+# Controls the maximum tfiles, ffiles, and afiles values reported by NFS FSSTAT calls through any associated mount targets.
+variable "max_fs_stat_files" {
+  description = "Maximum tfiles, ffiles, and afiles values reported by NFS FSSTAT"
+  default     = 223442
+  type        = number
+}
